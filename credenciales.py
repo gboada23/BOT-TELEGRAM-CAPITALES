b763a2e0776dfbbf56a08bc4428e1188f0cc5195
gboada23 <gustavoboadalugo@gmail.com>
2023-04-22 12:39:29 -0400

    V1.1

--- credenciales.py
+++ credenciales.py
@@ -6,4 +6,3 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(json, scope)
 gc = gspread.authorize(creds)
 token='6179451647:AAHhUOphBoMm4oGx_A2eDHJTPfzEW-fbVyE'
 key = '1rpT1LlBOKPv547CM4z3Z1Wn7AXF8ebfvPyMcP4McH4k'
-
